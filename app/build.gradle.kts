ecd35143ef1a201052434317c56a6bb742a7c9c6
DevOpsMoiz <moizsheikh276672@gmail.com>
2023-12-11 13:43:39 +0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+//    id("realm-android")
+}
+
+
+android {
+    namespace = "com.example.notes_taking"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.notes_taking"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("io.realm:realm-gradle-plugin")
+    implementation("io.realm:realm-android:0.82.1")
+    implementation("io.realm:realm-android-library:10.10.0")
+    implementation ("io.realm:realm-gradle-plugin:10.10.1")
+    implementation ("androidx.core:core-ktx:1.7.0")
+
+// Replace with the latest version
+
+}
